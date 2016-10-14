a36a6f5b112be04a7f1db4bd4c5cbb53a6c2198c
Kara Fulgum <karafulgum@gmail.com>
2016-10-14 13:26:47 -0500

    Work through tests for 04-reverse-list

--- app/exercise/reverse-list.js
+++ app/exercise/reverse-list.js
@@ -1,1 +1,7 @@
-export default null;
+export default function reverseList(list, destination) {
+  for (let i = list.children.length - 1; i >= 0; i--) {
+    const copy = list.children[i].cloneNode(true);
+
+    destination.appendChild(copy);
+  }
+}
--- app/exercise/sum-from-list.js
+++ app/exercise/sum-from-list.js
@@ -1,12 +1,11 @@
-// import message from './add-message-item';
-
 export default function sumFromList(list) {
   for (let i = 0; i < list.children.length; i++) {
-    console.log(list.children[i].innerText);
+    // console.log(list.children[i].innerText);
   }
   let count = 0;
   for (let i = 0; i < list.children.length; i++) {
     count = count + parseInt(list.children[i].innerText);
   }
+
   return count;
 }
