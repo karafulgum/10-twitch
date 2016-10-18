47164488a4e5060cfc0fbadc427581962f9c27d8
Kara Fulgum <karafulgum@gmail.com>
2016-10-18 15:35:53 -0500

    Tweaked the styling on the right sidebar

--- app/index.js
+++ app/index.js
@@ -5,10 +5,17 @@ import createGameElement from './twitch/create-game-element';
 
 
 // Look up a `ul` element with a class called `results`
-const ul = document.querySelector('.results');
+
 // console.log(data.top);
 // Add games to the results list based on the data from `data.json`
-for (let i = 0; i < data.length; i++) {
-  const results = data[i];
-  ul.appendChild(results);
-}
+// for (let i = 0; i < data.top.length; i++) {
+//   const ul = document.querySelector('.results');
+//   const games = data.top[i];
+//   const name = document.querySelector('game-item__name');
+//   name.innerText = games.name;
+  // const popularity = document.querySelector('game-item__popularity');
+  // popularity.innerText = games.popularity;
+  // const pic = document.querySelector('game-item__pic');
+  // pic.src.innerText = games.box.large;
+  // ul.appendChild(games);
+// }
--- app/twitch/add-games-to-list.js
+++ app/twitch/add-games-to-list.js
@@ -3,9 +3,19 @@ import createGameElement from './create-game-element';
 export default function showGamesInList(parentEl, data) {
   parentEl.innerHTML = '';
 
-  for (let i = 0; i < data.length; i++) {
-    const gameItem = data[i];
+  for (let i = 0; i < data.top.length; i++) {
+    const gameItem = data.top[i];
     const el = createGameElement(gameItem);
-    parentEl.appendChild(el);
+    const ul = document.querySelector('.results');
+    const name = document.querySelector('game-item__name');
+    name.innerText = gameItem.name;
+    ul.appendChild(el);
   }
 }
+
+// for (let i = 0; i < data.top.length; i++) {
+//   const ul = document.querySelector('.results');
+//   const games = data.top[i];
+//   const name = document.querySelector('game-item__name');
+//   name.innerText = games.name;
+// }
